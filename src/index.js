1c5c518be44de29f1c4cbc33a587accab23dd390
Tomas Pavlin <tomas.pavlin@gmail.com>
2021-02-22 21:03:56 +0100

    Integrate material design

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { CssBaseline } from '@material-ui/core';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <Provider store={store}>
       <App />
     </Provider>
